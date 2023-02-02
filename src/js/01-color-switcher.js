0c615c35860174034fa15cbbbd3673338e0844d5
BorysKhomitskyi <borys2130@gmail.com>
2023-02-02 07:59:38 +0200

    js-hw09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+const backgroundColorSwitcher = function () {
+  body.style.backgroundColor = getRandomHexColor();
+  body.style.color = getRandomHexColor();
+};
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(backgroundColorSwitcher, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+});
